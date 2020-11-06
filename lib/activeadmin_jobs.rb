1ccd5e9140525614bd5f6aeba94a44281d6c7774
AstraL <sawyer126@gmail.com>
2020-11-07 00:53:37 +0200

    Zeitwerk fix

--- lib/activeadmin_jobs.rb
+++ lib/activeadmin_jobs.rb
@@ -4,7 +4,8 @@ require "activeadmin"
 require "activeadmin_addons"
 require "activeadmin_jobs/engine"
 require "zeitwerk"
-#loader = Zeitwerk::Loader.for_gem
+
+loader = Zeitwerk::Loader.for_gem
 admin_dir = File.join(ActiveadminJobs::Engine.root, "app", "admin")
 config_ext = "#{__dir__}/activeadmin_jobs/activeadmin_config.rb"
 loader.push_dir(admin_dir)
