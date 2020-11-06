ef1fab184d6d552d3d8cdb7d5e9fd20d929de8a1
AstraL <sawyer126@gmail.com>
2020-11-06 23:10:20 +0200

    Zeitwerk fix

--- lib/activeadmin_jobs.rb
+++ lib/activeadmin_jobs.rb
@@ -3,6 +3,12 @@ require "devise"
 require "activeadmin"
 require "activeadmin_addons"
 require "activeadmin_jobs/engine"
+require "zeitwerk"
+loader = Zeitwerk::Loader.for_gem
+
+admin_ext = "#{__dir__}/app/admin/jobs.rb"
+loader.ignore(admin_ext)
+loader.setup
 
 module ActiveadminJobs
 end
